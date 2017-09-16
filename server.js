c7c6a96f53f7f5b6d6c80cd2723896228ff14f0f
Hiroyuki Inaba <hinaba@salesforce.com>
2017-09-16 16:33:39 +0900

    first release

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var port = process.env.PORT || 8081;
+var message = process.env.MESSAGE || '?';
+
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World. Sample01</h1>' + '<h1 style="color:block">' + message + '</h1>');
+}).listen(port);
