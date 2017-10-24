9f767718d4b8c34d41229aa32bdb44d9dc95fddc
Hiroyuki Inaba <hinaba@salesforce.com>
2017-10-24 11:04:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var message = process.env.MESSAGE || '?';
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello Devsumit.</h1>' + '<h1 style="color:Blue">' + message + '</h1>');
+  res.end('<h1>Hello Devsumit.</h1>' + '<h1 style="color:Black">' + message + '</h1>');
 }).listen(port);
